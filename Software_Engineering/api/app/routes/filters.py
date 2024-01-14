39e52bff93cf920fccb40a7dc2cd553d0e9d4c2b
abenazzou <adnanbenzo194@gmail.com>
2024-01-15 00:01:07 +0100

    Changed aggregation endpoint to match frontend needs

---
+++ Software_Engineering/api/app/routes/filters.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Request, HTTPException
+
+router = APIRouter()
+
+
+@router.get("/filters", response_description="List all unique filters options")
+async def list_filters(request: Request):
+    pipeline = [
+        {
+            "$group": {
+                "_id": None,
+                "categories": {"$addToSet": "$category"},
+                "subcategories": {"$addToSet": "$subcategory"},
+                "topics": {"$addToSet": "$topics"},
+                "authors": {"$addToSet": "$authors"}
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,  # Exclude the _id field
+                "categories": 1,
+                "subcategories": 1,
+                "topics": 1,
+                "authors": 1
+            }
+        }
+    ]
+   
+    try:
+        filters = await request.app.database["bbc-articles"].aggregate(pipeline).to_list(None)[0]
+        
+    except Exception as e:
+        print(f"Error querying database: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error while processing the request")
+
+    return filters
--- Software_Engineering/api/app/tests/test_list_categories.py
+++
@@ -1,6 +0,0 @@
-def test_list_categories_valid(client):
-    
-    response = client.get("/api/categories")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
-    
