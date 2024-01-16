505d620f7e1c1549a2441059ed734f2e33cd1ea2
abenazzou <adnanbenzo194@gmail.com>
2024-01-16 21:43:46 +0100

    Added trends endpoints for d3.js

--- Software_Engineering/api/app/routes/topics.py
+++ Software_Engineering/api/app/routes/topics.py
@@ -111,3 +111,99 @@ async def list_topics_frequency(request: Request,
         raise HTTPException(status_code=500, detail="Internal server error while processing the request")
 
     return topics
+
+
+@router.post("/topics/trends", response_description="Lists the trends of the top 10 topics options narrowed down by queries")
+async def list_topics_trends(request: Request,
+                    search_query: Optional[SearchQuery] = Body(default=None),
+                    categories: List[str] = Query(None),
+                    subcategories: List[str] = Query(None),
+                    authors: List[str] = Query(None),
+                    ):
+    
+    query = {}
+
+    if categories: query["category"] = {"$in": categories}
+    
+    if subcategories: query["subcategory"] = {"$in": subcategories}
+    
+    if authors: query["authors"] = {"$in": authors}
+    
+    if search_query and search_query.full_text_search:
+        query["$text"] = {"$search": f'\"{search_query.full_text_search}\"'}
+    
+    
+    topics_pipeline = [
+        {
+            "$match": query
+        },
+        {
+            "$unwind": "$topics"
+        },
+        {
+            "$group": {
+                "_id": "$topics",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+        "$project": {
+            "topic": "$_id",
+            "_id": 0,  # exclude the grouping _id
+            "count": 1
+        }
+    }
+    ]
+    
+    top_10_topics = await request.app.database["bbc-articles"].aggregate(topics_pipeline).to_list(10)
+    top_10_topics = [topic["topic"] for topic in top_10_topics]
+        
+    pipeline = [
+        {
+            "$unwind": "$topics"
+        },
+        {
+            "$match": {
+                "topics": {"$in": top_10_topics}
+            }
+        },
+        {
+            "$group": {
+                "_id": {
+                    "date": "$date_posted",
+                    "topic": "$topics"
+                },
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$group": {
+                "_id": "$_id.date",
+                "topics": {
+                    "$push": {
+                        "topic": "$_id.topic",
+                        "count": "$count"
+                    }
+                }
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "date": "$_id",
+                "topics": 1
+            }
+        }
+    ]
+    
+    try:
+        topics_trends = await request.app.database["bbc-articles"].aggregate(pipeline).to_list(None)
+        
+    except Exception as e:
+        print(f"Error querying database: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error while processing the request")
+
+    return topics_trends
