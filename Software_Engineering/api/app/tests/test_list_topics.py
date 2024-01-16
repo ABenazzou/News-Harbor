2e382678cff11ce9d5c8bc8cc474dd29ce04f9f9
abenazzou <adnanbenzo194@gmail.com>
2024-01-17 00:32:18 +0100

    Rollback from trends

--- Software_Engineering/api/app/tests/test_list_topics.py
+++ Software_Engineering/api/app/tests/test_list_topics.py
@@ -62,32 +62,3 @@ def test_list_topics_frequency_valid_with_params(client):
     assert isinstance(response_json, list)
     
 
-def test_list_topics_trends_valid_with_params(client):
-    
-    test_params = {
-        "categories": "World",
-        "subcategories": "Africa",
-    }
-    
-    query_params = []
-    for key, value in test_params.items():
-        if isinstance(value, list):
-            for item in value:
-                query_params.append(f"{key}={item}")
-        else:
-            query_params.append(f"{key}={value}")
-            
-    query_string = '&'.join(query_params)
-    
-    response = client.post(f"/api/topics/trends?{query_string}", json=test_params)
-    assert response.status_code == 200
-    response_json = response.json()
-    assert isinstance(response_json, list)
-    
-    
-def test_list_topics_trends_valid(client):
-    
-    response = client.post("/api/topics/trends")
-    assert response.status_code == 200
-    response_json = response.json()
-    assert isinstance(response_json, list)
